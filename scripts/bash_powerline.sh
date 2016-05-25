b3804a31b36126a085d4cbf0886f310616c76f83
Ariel Monaco <amonaco@gmail.com>
2016-05-25 15:47:57 +0900

    added bash powerline

---
+++ scripts/bash_powerline.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+curl https://raw.githubusercontent.com/riobard/bash-powerline/master/bash-powerline.sh > ~/.bash-powerline.sh
+echo source "~/.bash-powerline.sh" >> .bashrc 
+source .bashrc 
